3264d968a65e086e274c1f749b91b09e11d776b4
Pouyan <pouyannc@gmail.com>
2022-08-19 17:16:00 -0400

    Add function for summing all ints between two ints

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let sum = 0;
+    if (!(int1 >= 0) || !(int2 >= 0)) return "ERROR";
+    if (!(typeof(int1) == "number") || !(typeof(int2) == "number")) return "ERROR";
+
+    let [smallInt, bigInt] = (int1 > int2) ? [int2, int1] : [int1, int2];
+    for (let i = smallInt; i <= bigInt; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
