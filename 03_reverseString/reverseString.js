a282a9c3253b49ee22bf231d1bb0ebbf3debd33a
Pouyan <pouyannc@gmail.com>
2022-08-19 15:02:38 -0400

    Make function to reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let reverseArray = [];
+    for (let i = stringArray.length-1; i >= 0; i--) {
+        reverseArray.push(stringArray[i]);
+    }
+    return reverseArray.join("");
 };
 
 // Do not edit below this line
