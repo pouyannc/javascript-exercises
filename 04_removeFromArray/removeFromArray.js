804650845bd6d989433e97a71c920d4fb2152e0a
Pouyan <pouyannc@gmail.com>
2022-08-19 16:40:09 -0400

    Make array filter function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    const filteredArray = array.filter((i) => {
+        for (let x = 1; x < arguments.length; x++){
+            if (i === arguments[x]) return false
+            else continue; 
+        }
+        return true;
+    })
+    return filteredArray;
 };
 
 // Do not edit below this line
