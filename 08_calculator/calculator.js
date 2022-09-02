9774e74e3b6a2226204be3ccd180c7774dc4bb62
Pouyan <pouyannc@gmail.com>
2022-09-01 20:31:33 -0400

    Complete calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return parseInt(a) + parseInt(b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return parseInt(a) - parseInt(b);
 };
 
-const sum = function() {
-	
+const sum = function(numList) {
+	const sum = numList.reduce((total, nextNum) => {return total + nextNum}, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numList) {
+  const product = numList.reduce((total, nextNum) => {return total * nextNum});
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fac = 1;
+  for (let i = 1; i <= a; i++) {
+    fac *= i;
+  }
+  return fac;
 };
 
 // Do not edit below this line
