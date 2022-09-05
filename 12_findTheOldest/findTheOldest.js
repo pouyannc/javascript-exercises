d3ca7072bf22a53e7f3ac43295d5ca3469e15f33
Pouyan <pouyannc@gmail.com>
2022-09-05 14:12:15 -0400

    Function queries for the oldest person in object array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let currentTime = new Date();
+    people.forEach(function(person) {
+        if (person.hasOwnProperty('yearOfDeath')){
+            person.age = (person.yearOfDeath-person.yearOfBirth);
+        } else person.age = currentTime.getFullYear() - person.yearOfBirth;
+    });
+    console.log(people.sort((a,b) => b.age - a.age));
+    return people[0];
 };
 
 // Do not edit below this line
