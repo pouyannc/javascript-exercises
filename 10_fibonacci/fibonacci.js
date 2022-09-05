fc24f4106190a8d94afe8320d07ffc41cf17f4ca
Pouyan <pouyannc@gmail.com>
2022-09-04 22:45:23 -0400

    Fibonacci number search

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 1) return "OOPS";
+    let fib = [0,1,1];
+    for (let i = 3; i<=n; i++) {
+        fib.push(fib[i-2]+fib[i-1]);
+    }
+    return fib[n];
 };
 
 // Do not edit below this line
