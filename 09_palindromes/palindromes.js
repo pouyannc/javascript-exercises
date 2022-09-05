d61ad246504b6c3f0cd4333f4bd40485e82dc214
Pouyan <pouyannc@gmail.com>
2022-09-04 22:23:09 -0400

    Function that checks for palidrome strings

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const filteredStr = [...string].filter(char => char.search(regArg) != -1).join("").toLowerCase();
+    const reverseFilteredStr = [...filteredStr].reverse().join("");
+    return (filteredStr == reverseFilteredStr)
 };
+
+const regArg = /[a-z]/ig;
 
 // Do not edit below this line
 module.exports = palindromes;
